b6d43b16c04dfe90b1d7f2fe857e9f693293a4e7
AdaYusoff2014831292 <adayusoff@yahoo.com>
2015-10-11 15:37:39 +0800

    assignment1
    python write read in java

---
+++ python_read.py
@@ -0,0 +1,6 @@
+print "My Biodata"
+
+text_file = open("Rabbil_Atul.txt", "r")
+print text_file.read()
+text_file.close()
+
---
+++ python_write.py
@@ -0,0 +1,3 @@
+text_file = open("Rabbil_Atul.txt","w")
+text_file.write('Name Rabbil Atul Hadawiah Binti Che Yusoff,Matrix No 2014831292,Gender Female,Email adayusoff@yahoo.com')
+text_file.close()
